8f97e49aba5156794550c361030c72d9330a38a9
drewgifford <drew@drewgifford.com>
2025-04-01 14:40:39 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex items-center h-lvh w-full">
+
+    <div class="mx-auto">
+      <div>
+        <img src="./assets/logo.svg" class="w-52 mx-auto bg-white p-6 rounded-lg"/>
+      </div>
+      
+
+      <h1 class="text-6xl flex-1 mt-4 text-white">COMING SOON.</h1>
+    </div>
+    
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
